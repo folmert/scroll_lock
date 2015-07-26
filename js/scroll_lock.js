1a6f5f8552e1aa5eae3dbc9db3119d000d37d7eb
folmert <folmert2@gmail.com>
2015-07-26 14:26:16 +0200

    minor fixes after jshint

--- js/scroll_lock.js
+++ js/scroll_lock.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var scroll_lock = document.querySelectorAll('.scroll_lock');
 
@@ -7,13 +7,13 @@ if(document.querySelectorAll('.scroll_lock').length) {
 	var i = 0;
 	for (var i = scroll_lock.length - 1; i >= 0; i--) {
 
-		scroll_lock[i].addEventListener('click', function(e) {
+		scroll_lock[i].addEventListener('click', function() {
 			this.classList.add('js-scroll_lock_removed');
 		},false);
 
-		scroll_lock[i].addEventListener('mouseleave', function(e) {
+		scroll_lock[i].addEventListener('mouseleave', function() {
 			this.classList.remove('js-scroll_lock_removed');
 		},false);
 
-	};
+	}
 }
